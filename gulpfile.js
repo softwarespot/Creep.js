fcb31c45cea6724fbbc1e90112ce83e629c8dd82
softwarespot <softwarespotapps@gmail.com>
2015-12-08 18:29:26 +0200

    Updated gulp

--- gulpfile.js
+++ gulpfile.js
@@ -7,16 +7,32 @@ var rename = require('gulp-rename');
 var replace = require('gulp-replace');
 var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
+var del = require('del');
 var fs = require('fs');
 
+// Assets for the project
+var Assets = {
+    css: {
+        main: './example.scss',
+        compiled: './example.css',
+        source: './',
+    },
+    js: {
+        main: './jquery.creep.js',
+        minified: './jquery.creep.min.js',
+        source: './',
+    },
+    package: './package.json',
+};
+
 // See the saas documentation for more details
-var saasSettings = {
+var _saasSettings = {
     // Options are 'nested', 'compact', 'compressed', 'expanded'
     outputStyle: 'compressed',
 };
 
 // See the uglify documentation for more details
-var uglifySettings = {
+var _uglifySettings = {
     compress: {
         comparisons: true,
         conditionals: true,
@@ -29,77 +45,70 @@ var uglifySettings = {
     },
 };
 
-// Assets for the project
-var Assets = {
-    css: {
-        main: 'example.scss',
-        compiled: 'example.css',
-    },
-    js: {
-        main: 'jquery.creep.js',
-        minified: 'jquery.creep.min.js',
-    },
-    package: 'package.json',
-};
+// Clean the current directory
+gulp.task('clean', function clesnTask(cb) {
+    del([Assets.js.minified], cb);
+});
 
 // Check the main js file meets the following standards outlined in .jshintrc
-gulp.task('jshint', function() {
-    return gulp.src('./' + Assets.js.main)
+gulp.task('jshint', function jsHintTask() {
+    return gulp.src(Assets.js.main)
         .pipe(jshint())
         .pipe(jshint.reporter('jshint-stylish'));
 });
 
 // Prettify the main js file
-gulp.task('prettify-js', function() {
+gulp.task('prettify-js', function prettifyJSTask() {
     gulp.src(Assets.js.main)
         .pipe(prettify({
             config: '.jsbeautifyrc',
             mode: 'VERIFY_AND_WRITE',
         }))
-        .pipe(gulp.dest('./'));
+        .pipe(gulp.dest(Assets.js.source));
 });
 
 // Compile the main scss (saas) stylesheet
-gulp.task('saas', function() {
+gulp.task('saas', function saasTask() {
     return gulp.src(Assets.css.main)
-        .pipe(sass(saasSettings))
+        .pipe(sass(_saasSettings))
         .pipe(rename(Assets.css.compiled))
-        .pipe(gulp.dest('./'));
+        .pipe(gulp.dest(Assets.css.source));
 });
 
 // Uglify aka minify the main js file
-gulp.task('uglify', function() {
-    return gulp.src('./' + Assets.js.main)
-        .pipe(uglify(uglifySettings))
+gulp.task('uglify', function uglifyTask() {
+    return gulp.src(Assets.js.main)
+        .pipe(uglify(_uglifySettings))
         .pipe(rename(Assets.js.minified))
-        .pipe(gulp.dest('./'));
+        .pipe(gulp.dest(Assets.js.source));
 });
 
 // Update version numbers based on the main file version comment
-gulp.task('version', function() {
+gulp.task('version', function versionTask() {
     // SemVer matching is done using (?:\d+\.){2}\d+
 
+    var VERSION_NUMBER = 1;
     var reVersion = /\n\s*\*\s+Version:\s+((?:\d+\.){2}\d+)/;
-    var version = fs.readFileSync('./' + Assets.js.main, {
+    var version = fs.readFileSync(Assets.js.main, {
         encoding: 'utf8',
     })
 
     // Match is found in the 2nd element
-    .match(reVersion)[1];
+    .match(reVersion)[VERSION_NUMBER];
 
     // package.json version property
-    return gulp.src('./' + Assets.package)
+    return gulp.src(Assets.package)
         .pipe(replace(/"version":\s+"(?:\d+\.){2}\d+",/, '"version": "' + version + '",'))
-        .pipe(gulp.dest('./'));
+        .pipe(gulp.dest(Assets.js.source));
 });
 
 // Register the default task
-gulp.task('build', ['jshint', 'saas', 'version', 'uglify', 'prettify-js']);
+gulp.task('build', ['jshint', 'saas', 'version', 'clean', 'uglify', 'prettify-js']);
 
 // Watch for changes to the js and scss files
-gulp.task('default', function() {
-    gulp.watch('./' + Assets.css.main, ['saas']);
-    gulp.watch('./' + Assets.js.main, ['version', 'jshint', 'uglify']);
+gulp.task('default', function defaultTask() {
+    gulp.watch(Assets.css.main, ['saas']);
+    gulp.watch(Assets.js.main, ['version', 'jshint', 'clean', 'uglify']);
 });
 
 // 'gulp build' to invoke all tasks above
